68c6ed5f8789d600ddb0c3b4e0193a733196107b
Hardeep Singh <hs0774@gmail.com>
2023-11-20 00:20:57 -0500

    more packages and set up for form created

--- models/userAccount.js
+++ models/userAccount.js
@@ -6,6 +6,7 @@ const userAccountSchema = new Schema ({
    firstName:{type:String,minLength:8,maxLength:15,required:true},
    lastName:{type:String,minLength:8,maxLength:15,required:true},
    email:{type:String,match:/^\S+@\S+\.\S+$/,required:true,unique:true},
+   password:{type:String,minLength:8,required:true},
    messages:[{type:Schema.Types.ObjectId,ref:"Message"}],
    member:{type:Boolean,default:false},
    admin:{type:Boolean,default:false},
@@ -16,7 +17,9 @@ userAccountSchema.virtual("fullname").get(function(){
 })
 
 userAccountSchema.virtual("url").get(function(){
-    return `/board/user/${this._id}`
+    return `/board/account/${this._id}`
 })
 
 module.exports = mongoose.model('Account',userAccountSchema);
+
+//preHook?
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+//form test
+router.get('/sign-up', function(req, res, next) {
+  res.render('sign-up');
+});
+
+//form test post
+router.post('/sign-up', function(req, res, next) {
+  
 });
 
 module.exports = router;
